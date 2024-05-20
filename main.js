4610f6a4e68791244acf7a2e2a13000416437972
jose-g315 <120354063+jose-g315@users.noreply.github.com>
2024-05-20 01:59:10 -0500

    Added create new grid and reset functionality.

--- main.js
+++ main.js
@@ -1,29 +1,61 @@
 //jose-g315
 //etch-a-sketch game logic and DOM manipulation
 
-const container = document.querySelector(".container");
-const gridDiv = [];
-const tileDiv = [];
-
 function createGrid(size){
+    const container = document.querySelector(".sketchPad");
+    const rowDiv = [];
+    const tileDiv = [];
+    // creating grid based on size input
     for (i=0;i<size;i++){
-        gridDiv[i] = document.createElement('div');
-        //gridDiv[i].textContent = "ROW";
-        gridDiv[i].classList.add("row");
+        // creating each row
+        rowDiv[i] = document.createElement('div');
+        rowDiv[i].classList.add("row");
         for (j=0;j<size;j++) {
+            // creating each tile
             tileDiv[i] = document.createElement('div');
-            tileDiv[i].textContent = "";
             tileDiv[i].classList.add("tile");
-            gridDiv[i].append(tileDiv[i]);
+            rowDiv[i].append(tileDiv[i]);
         }
-        container.append(gridDiv[i]);
+        container.append(rowDiv[i]);
+        activatePen();
     }
 }
-createGrid(8);
+// 16x16 grid created as default
+createGrid(16);
 
-const tiles = document.querySelectorAll(".tile");   
-tiles.forEach(tile => tile.addEventListener("mouseover", ()=> {
-    console.log("test");
-    tile.classList.add('hover');
-    
-}));
+// event listener wrapped in a function that add the hover class to each tile
+function activatePen(){
+    const tiles = document.querySelectorAll(".tile");
+    tiles.forEach(tile => tile.addEventListener("mouseover", ()=> {
+    tile.classList.add("hover");
+    }));
+}
+
+// event listener that destroys old grid and creates a new one
+const newGridButton = document.querySelector(".newGridButton");
+newGridButton.addEventListener("click", () => {
+    const rows = document.querySelectorAll(".row");
+    const tiles = document.querySelectorAll(".tile");
+    let size = prompt("Enter a Grid Size from 2-100");
+    // input validation
+    if (size >= 2 && size <= 100){
+        // removing all rows and tiles from previous grid
+        rows.forEach(row => row.remove());
+        tiles.forEach(tile => tile.remove());
+        // creating new grid
+        createGrid(size);
+    } else if (size === null) {
+        return;
+    } else {
+        alert("Invalid")
+    }
+});
+
+// event listener that resets the current grid
+const resetButton = document.querySelector(".resetButton");
+resetButton.addEventListener("click", () => {
+    const tiles = document.querySelectorAll(".tile");
+    // removing hover class which makes the tiles white again
+    tiles.forEach(tile => tile.classList.remove("hover"));
+    activatePen();
+});
