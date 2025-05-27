a4aa50a14ab1ff130f22cdf60dd522e4705b504f
jose-g315 <120354063+jose-g315@users.noreply.github.com>
2025-05-27 11:31:42 -0500

    Refactor javascript to be more efficient and easier to read

--- main.js
+++ main.js
@@ -1,67 +1,75 @@
 //jose-g315
 //etch-a-sketch game logic and DOM manipulation
 
-function createGrid(size){
-    const container = document.querySelector(".sketch-pad");
-    const rowDiv = [];
-    const tileDiv = [];
-    // creating grid based on size input
+const sketchPad = document.querySelector(".sketch-pad");
+const btnContainer = document.querySelector(".button-container");
+
+function createGrid(size){   
+    /* creating grid by making n rows with n tiles in each row 
+       based on input size */
     for (i=0;i<size;i++){
-        // creating each row
-        rowDiv[i] = document.createElement('div');
-        rowDiv[i].classList.add("row");
+        const row = document.createElement('div');
+        row.classList.add("row");
+        sketchPad.appendChild(row);
         for (j=0;j<size;j++) {
-            // creating each tile
-            tileDiv[i] = document.createElement('div');
-            tileDiv[i].classList.add("tile");
-            rowDiv[i].append(tileDiv[i]);
+            const tile = document.createElement('div');
+            tile.classList.add("tile");
+            row.appendChild(tile);
         }
-        container.append(rowDiv[i]);
         activatePen();
     }
 }
-// 16x16 grid created as default
-createGrid(16);
 
 function createRandomColor(){
     let r = Math.random() * (256 - 0) + 0;
     let g = Math.random() * (256 - 0) + 0;
     let b = Math.random() * (256 - 0) + 0;
-    let rgb = "rgb("+r+","+g+","+b+")";
+    let rgb = `rgb(${r},${g},${b})`;
     return rgb;
 }
-// event listener wrapped in a function that add the hover class to each tile
+
 function activatePen(){
     const tiles = document.querySelectorAll(".tile");
-    tiles.forEach(tile => tile.addEventListener("mouseover", ()=> {
-    tile.style.background = createRandomColor();
-    }));
+    for (const tile of tiles) {
+        tile.addEventListener("mouseover", ()=> {
+            tile.style.background = createRandomColor();
+            })
+    };
 }
 
-// event listener that destroys old grid and creates a new one
-const newGridButton = document.querySelector(".newGridButton");
-newGridButton.addEventListener("click", () => {
+function destroyGrid(){
     const rows = document.querySelectorAll(".row");
+    for(const row of rows) {
+        row.remove();
+    };
+}
+
+function resetGrid() {
     const tiles = document.querySelectorAll(".tile");
-    let size = prompt("Enter a Grid Size from 2-100");
-    // input validation
-    if (size >= 2 && size <= 100){
-        // removing all rows and tiles from previous grid
-        rows.forEach(row => row.remove());
-        tiles.forEach(tile => tile.remove());
-        // creating new grid
-        createGrid(size);
-    } else if (size === null) {
-        return;
-    } else {
-        alert("Invalid")
+    for (const tile of tiles) {
+        tile.style.background = "whitesmoke";
+    };
+}
+
+btnContainer.addEventListener("click", (e) => {
+    switch(e.target.className) {
+        case "reset-button":
+            resetGrid();
+            break;
+        case "new-grid-button":
+            let size = prompt("Enter a Grid Size from 2-100");
+            // input validation
+            if (size >= 2 && size <= 100){
+                destroyGrid();
+                createGrid(size);
+            } else if (size === null) {
+                return;
+            } else {
+                alert("Invalid")
+            }
+            break;
     }
 });
 
-// event listener that resets the current grid
-const resetButton = document.querySelector(".resetButton");
-resetButton.addEventListener("click", () => {
-    const tiles = document.querySelectorAll(".tile");
-    tiles.forEach(tile => tile.style.background = "whitesmoke");
-    activatePen();
-});
+// 16x16 grid created as default
+createGrid(16);
